07475c242d496d03f2ec92bcb8f17629b46a7058
Damien McMahon <damien.mcmahon@zonedigital.com>
2019-05-20 10:41:47 +0100

    Adding standard response shape and states.json

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const { tenants } = require('./tenant');
 const { createParties } = require('./party');
 const currencies = require('./json/currencies');
 const countries = require('./json/countries');
+const states = require('./json/states');
+
+const standardResp = (key, data) => ({
+  "status": [{
+    "majorCode": "0",
+    "minorCode": "0",
+    "level": "S",
+    "message": "Action Successful"
+  }],
+  [key]: data
+});
 
 module.exports = () => {
   const data = {
-    users: createUsers(10),
-    tenants,
-    parties: createParties(10, tenants),
-    currencies,
-    countries,
-    queue: createParties(5, tenants),
+    "countries": standardResp('countries', countries),
+    "currencies": standardResp('currencies', currencies),
+    "parties": standardResp('parties', createParties(10, tenants)),
+    "queue": standardResp('queue', createParties(5, tenants)),
+    "states": standardResp('states', states),
+    "tenants": standardResp('tenants', tenants),
+    "users": standardResp('users',createUsers(10)),
   };
 
   return data;
